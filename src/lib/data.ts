b72d7d608da75d5101e364b2e3214f4b5264111b
kBashar <khyrulbashar.0.1@gmail.com>
2025-05-19 22:44:01 +0600

    data updated

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,168 +1,129 @@
 export const personalInfo = {
-  name: "Khyrul Bashar",
-  location: "Dhaka, Bangladesh",
-  email: "khyrulbashar.0.1@gmail.com",
-  github: "https://github.com/kBashar",
-  linkedin: "https://www.linkedin.com/in/khyrulbashar",
-};
-
-export const workExperience = [
-  {
-    company: "Chatbyte GmbH",
-    location: "Remote",
-    position: "Software Engineer",
-    period: "Mar 2024 - Present",
-    achievements: [
-      "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
-      "Built a comprehensive admin panel for managing content, user data, and analytics.",
-      "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
-      "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
-      "Drove the integration of a custom affiliate marketing system, enabling seamless tracking and reporting of referrals and user activities.",
-      "Integrated Text-to-Speech (TTS) services for enhanced user experiences, optimizing both frontend and backend systems.",
-      "Collaborated with cross-functional teams to deliver high-quality features, actively reviewing pull requests to ensure code quality, adherence to standards, and efficient implementation.",
+    name: "Khyrul Bashar Jubayer",
+    location: "Dhaka, Bangladesh",
+    email: "khyrulbashar.0.1@gmail.com",
+    github: "https://github.com/kBashar",
+    linkedin: "https://www.linkedin.com/in/khyrulbashar",
+    portfolio: "https://kbashar.github.io",
+    phone: "+8801840007712",
+    leetcode: "https://leetcode.com/u/kBashar/",
+    hackerrank: "https://www.hackerrank.com/kbashar",
+    medium: "https://medium.com/@kbashar"
+  };
+  
+  export const workExperience = [
+    {
+      company: "Gigalogy Inc",
+      location: "Dhaka, Bangladesh",
+      position: "Software Engineer",
+      period: "Sep 2023 - Dec 2024",
+      achievements: [
+        "Led the backend development of a Retrieval Augmented Generation (RAG) SaaS platform and assisted frontend teams in designing and developing client services.",
+        "Designed a concept of profiles for reusable and customizable GPT personas, reducing prompt experimentation to deployment time by 90%.",
+        "Developed an empathetic and responsive product recommendation system based on LLM, deployed in 5+ Japanese e-commerce sites.",
+        "Mentored junior engineers, improving team development practices.",
+      ],
+      techStack: ["LLM", "OpenAI", "Anthropic", "FastAPI", "PostgreSQL", "ElasticSearch", "Qdrant"]
+    },
+    {
+      company: "Unipolar Automation Technologies",
+      location: "Dhaka, Bangladesh",
+      position: "R&D Team Lead",
+      period: "Apr 2022 - Aug 2023",
+      achievements: [
+        "Led a 6-member IoT team and launched 4 industrial products.",
+        "Created a synchronous weight logging system, boosting tracking and reducing errors by 30%.",
+        "Built a desktop power failure monitoring app for DPDC.",
+        "Developed OPC-UA protocol libraries to improve integration by 40%."
+      ],
+      techStack: ["Django", "PostgreSQL", "AWS EC2", "QT PySide6", "Docker", "Websocket", "MongoDB"]
+    },
+    {
+      company: "Unipolar Automation Technologies",
+      location: "Dhaka, Bangladesh",
+      position: "Software Engineer",
+      period: "Jul 2019 - Mar 2022",
+      achievements: [
+        "Created a web-based GIS to visualize municipal water supply.",
+        "Developed a desktop app for concrete mixing plant operations."
+      ]
+    },
+    {
+      company: "Government Munshiganj Polytechnic Institute",
+      location: "Munshiganj, Bangladesh",
+      position: "Computer Technology Instructor",
+      period: "Sep 2017 - Jul 2019",
+      achievements: [
+        "Taught Data Structures, Networking, and Database courses to diploma students."
+      ]
+    },
+    {
+      company: "Apache Foundation, Google Summer of Code",
+      location: "Remote",
+      position: "Software Engineering Intern",
+      period: "Apr 2015 - Aug 2015",
+      achievements: [
+        "Developed a Java Swing desktop tool for debugging PDF files with visual and hex editors.",
+        "Project link: https://issues.apache.org/jira/browse/PDFBOX-2530"
+      ]
+    }
+  ];
+  
+  export const education = [
+    {
+      institution: "Chittagong University of Engineering and Technology (CUET)",
+      location: "Chittagong, Bangladesh",
+      degree: "B.Sc. in Computer Science and Engineering",
+      period: "2011-2015",
+      achievements: [
+        "Organized coding popularization events for high school students.",
+        "Convener of Firefox day-long app development hackathon (2013)."
+      ]
+    }
+  ];
+  
+  export const skills = {
+    programmingLanguages: [
+      "Python",
+      "JavaScript",
+      "TypeScript",
+      "Java",
+      "C++",
     ],
-  },
-  {
-    company: "DevCrew",
-    location: "Coimbatore, India",
-    position: "Software Engineer",
-    period: "Mar 2023 - Mar 2024",
-    achievements: [
-      "Developed responsive frontend components using React.js, enhancing user experience and app performance.",
-      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+    frameworksAndLibraries: [
+      "FastAPI",
+      "Django",
+      "React",
     ],
-  },
-  {
-    company: "Phoenitags",
-    location: "Coimbatore, India",
-    position: "Software Engineering Intern",
-    period: "Jan 2023 - Mar 2023",
-    achievements: [
-      "Built interactive UIs with React.js, focusing on reusable components and performance optimization.",
-      "Implemented JavaScript-based features like user authentication and real-time updates.",
+    databases: [
+      "PostgreSQL",
+      "MongoDB",
+      "ElasticSearch",
+      "Redis",
+      "Qdrant"
     ],
-  },
-];
-
-export const education = [
-  {
-    institution: "Sri Krishna College of Engineering and Technology",
-    location: "Coimbatore, India",
-    degree: "B Tech Information Technology",
-    period: "Jun 2021 - Jun 2025",
-    achievements: [
-      "President of the Department of Information Technology",
-      "G20 student delegate",
-      "Organized multiple technical workshops and events",
-      "Represented my college in multiple national and international hackathons and won.",
-    ],
-  },
-];
-export const skills = {
-  programmingLanguages: [
-    "TypeScript",
-    "JavaScript",
-    "Python",
-    "C++",
-    "Java",
-    "Solidity",
-  ],
-  frontendDevelopment: [
-    "Nextjs",
-    "Reactjs",
-    "React Native",
-    "Shadcn UI",
-    "Tailwind CSS",
-    "HTML",
-    "CSS",
-  ],
-  backendDevelopment: ["Nodejs", "Expressjs"],
-  databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
-  cloudAndDevOps: ["AWS"],
-  toolsAndServices: [
-    "Clerk (Auth)",
-    "Sanity (CMS)",
-    "Tinybird (analytics)",
-    "Zod",
-    "Sentry",
-    "Mixpanel",
-    "Trigger.dev",
-  ],
-};
-
-export const projects = [
-  {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
-    description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
-    ],
-  },
-  {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
-    description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
-    ],
-  },
-];
-
-export const awards = [
-  {
-    name: "IEEE YESIST12 Hackathon",
-    issuer: "IEEE",
-    date: "Sep 2022",
-    type: "International",
-    position: "Second Place",
-  },
-  {
-    name: "Prodigi Cognizant Hackathon",
-    issuer: "Cognizant",
-    date: "Feb 2023",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Cisco Thingqbator Hackathon",
-    issuer: "Cisco",
-    date: "Jan 2023",
-    type: "National",
-    position: "First Runner-up",
-  },
-  {
-    name: "Innovators Day",
-    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
-    date: "Sep 2022",
-    type: "National",
-    position: "First Prize",
-  },
-  {
-    name: "KG Hackfest'22",
-    issuer: "KGiSL Institute of Technology, Coimbatore",
-    date: "Sep 2022",
-    type: "National",
-    position: "Second Prize",
-  },
-  {
-    name: "Innohacks'22",
-    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
-    date: "May 2022",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Hack @ SKCET",
-    issuer: "Hackclub SKCET, SKCET, Coimbatore",
-    date: "Feb 2022",
-    type: "National",
-    position: "Most Impactful Hack",
-  },
-];
+    cloudAndInfra: [
+      "Docker",
+      "Linux",
+      "Github Actions (CI/CD)",
+      "AWS (ECS, EC2, S3, Lambda, LightSail, Amplify)"
+    ]
+  };
+  
+  export const awards = [
+    {
+      name: "International Contest on Programming & Systems Development (ICPSD’14)",
+      issuer: "GIBS",
+      date: "Aug 2014",
+      type: "International",
+      position: "Second Runner-up"
+    },
+    {
+      name: "National Android Apps Development Training Performance Evaluation",
+      issuer: "Ministry of Information and Technology",
+      date: "May 2014",
+      type: "National",
+      position: "First Runner-up"
+    }
+  ];
+  
